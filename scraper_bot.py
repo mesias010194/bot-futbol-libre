38f00b65c1367fdbaca873aaa5dd6326b511f664
mesias010194 <mesias.lic@gmail.com>
2026-06-27 00:49:39 -0500

    Update scraper_bot.py

--- scraper_bot.py
+++ scraper_bot.py
@@ -298,7 +298,7 @@ def actualizar_nube(datos):
         print("[*] Conectando con GitHub API...")
         
         # === CONFIGURACIÓN GITHUB ===
-        github_token = "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" 
+        github_token = "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" 
         repo = "mesias010194/bot-futbol-libre"
         file_path = "agenda.json"
         url = f"https://api.github.com/repos/{repo}/contents/{file_path}"
